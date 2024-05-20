0f5f6784519c0b44229ff08939d0baa820527624
Daniel Kao <daniel.kao@gmail.com>
2024-05-20 23:12:01 +0800

    make highlighted word in bold

--- dictdialog.lua
+++ dictdialog.lua
@@ -1,6 +1,7 @@
 local InputDialog = require("ui/widget/inputdialog")
 local ChatGPTViewer = require("chatgptviewer")
 local UIManager = require("ui/uimanager")
+local TextBoxWidget = require("ui/widget/textboxwidget")
 local _ = require("gettext")
 
 local queryChatGPT = require("gpt_query")
@@ -23,18 +24,18 @@ local function showChatGPTDialog(ui, highlightedText, message_history)
               content = 
                   prev_context .. "<<" .. highlightedText .. ">>" .. next_context .. "\n" ..
                   "explain vocabulary or content in <<>> in above sentence, in zh-TW with following format\n" .. 
-                  "1. explanation\n" ..
-                  "2. vocabulary original form. if it's Japanese, show its hiragana spelling\n" ..
-                  "3. give an example in original language; and also attach zh-tw translation in second line\n" ..
+                  "1.vocabulary original form. if it's Japanese, show its hiragana spelling; if it's other languages, show original spelling. for example: went -> go; ran -> run\n" ..
+                  "2.explanation of content in <<>> according to context\n" ..
+                  "3.give an example in original language; and also attach zh-tw translation in second line\n" ..
                   "only show replies; no extra description nor information\n" ..
                   "----\n" ..
                   "here's an example\n" ..
                   "人の心は<<読める>>か？" ..
                   "and here's what to output:" ..
-                  "1.指能夠理解或猜測他人的想法或情感。\n"..
-                  "2.読める（よめる）\n"..
-                  "3.彼の表情から彼女の気持ちが読める。\n"..
-                  "  從他的表情可以讀出她的心情。\n",
+                  "読める（よめる）\n"..
+                  "指能夠理解或猜測他人的想法或情感。\n"..
+                  "例:彼の表情から彼女の気持ちが読める。\n"..
+                  "(從他的表情可以讀出她的心情。)\n",
             }
             table.insert(message_history, context_message)
 
@@ -48,7 +49,8 @@ local function showChatGPTDialog(ui, highlightedText, message_history)
             table.insert(message_history, answer_message)
             UIManager:close(input_dialog)
             local result_text = 
-              prev_context .. "<<" .. highlightedText .. ">>" .. next_context .. "\n\n" ..
+              TextBoxWidget.PTF_HEADER .. 
+              prev_context .. TextBoxWidget.PTF_BOLD_START .. highlightedText .. TextBoxWidget.PTF_BOLD_END .. next_context .. "\n\n" ..
               answer
 
             local function createResultText(highlightedText, message_history)
